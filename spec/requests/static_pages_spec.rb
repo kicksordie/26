1341ec1350f96660ef6ae4953c6c57b1ca59d84d
Oregand <davidoregan91@gmail.com>
2014-08-11 17:55:53 +0100

    Add bootstrap, using asset pipeline & refinding scss

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_title("#{base_title} | Home")
     end
+
+    it "should have custom page title " do
+      visit 'static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
